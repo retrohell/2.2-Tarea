ea8ec56bdf2adab49816e3b8809c4be4f3b600e6
IRVIN DANIEL CAMPOS VALENZUELA <a329762@uach.mx>
2021-11-16 23:31:16 -0700

    15.js

---
+++ 15.js
@@ -0,0 +1,19 @@
+var teclado = 0, teclado2 = 0, num = 0, tot = 0;
+process.stdout.on('data', function (data) {
+    teclado = data;
+    process.exit();
+});
+process.stdout.on('data', function (data) {
+    teclado2 = data;
+    process.exit();
+});
+
+for(let i=teclado; i<=teclado2; i++){
+    console.log(i);
+    if(i%2==0 || i%3==0){
+        num++;
+    }
+    tot++;
+}
+console.log("existen ", num, " pares");
+console.log("un total de ", tot);
